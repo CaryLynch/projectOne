3a7452a5e069a8e6876a891e0c35d97a371c8249
Cary Lynch <carysada@gmail.com>
2015-04-19 17:29:50 -0400

    completed number 1 in backlog in trello

--- forumApp.js
+++ forumApp.js
@@ -30,19 +30,42 @@ app.get('/topics', function(req, res){
   });
 });
 
+app.get('/topics/new', function(req, res){
+  var read = fs.readFileSync('./views/topics/new.html', 'utf8');
+  res.send(read);
+}); 
+
+// this post inserts a new topic into the topic table and then redirects to the list page (get('.topics'))
+app.post('/topics/create', function(req, res){
+  console.log(req.body);
+  db.run("INSERT INTO topics (title, body, vote, author) VALUES ('" + req.body.title + "', '" + req.body.body + "', '" + req.body.vote + "', '" + req.body.author + "')");
+  res.redirect('/topics');
+}); 
+
+// this post inserts a new comment into the comment table and then redirects to the list page (get('.topics'))
+app.post('/topics/:id/comment', function(req, res){
+  var id = req.params.id;
+  console.log("heres the" + id);
+  db.run("INSERT INTO comments (comment, topics_id) VALUES ('" + req.body.comments + "', " + id + ");");
+  res.redirect('/topics');
+}); 
 
 // page that displays the individual topics and their comments with the ability to vote for that topic and add a comment
 app.get('/topics/:id', function(req, res){
   var id = req.params.id;
+  // var renderedHTML;
   db.all("SELECT * FROM topics WHERE id = " + id + ";", {}, function(err, topic){
-    fs.readFile('./views/topics/show.html', 'utf8', function(err, temp){
-      console.log(topic);
-        var renderedHTML = Mustache.render(temp, {"title":topic[0].title, "body":topic[0].body, "author":topic[0].author});
-
-        res.send(renderedHTML);
+    db.all("SELECT * FROM comments WHERE topics_id = " + id + ";", {}, function(err, comments){
+          console.log(comments);
+          fs.readFile('./views/topics/show.html', 'utf8', function(err, temp){
+            console.log(topic);
+            renderedHTML = Mustache.render(temp, {id:topic[0].id, title:topic[0].title,  body:topic[0].body, author:topic[0].author, comments: comments});
+             res.send(renderedHTML);
+         });     
+      });
     });
-  });
 });
+
 
 
 app.listen(3000, function() {
